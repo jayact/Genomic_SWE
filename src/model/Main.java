0300274feeff3d801dfbeb71490d96a3d201c577
creigh48 <creigh48@students.rowan.edu>
2014-03-27 18:08:14 -0400

    Update Main.java

--- src/model/Main.java
+++ src/model/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, world!");
+		System.out.println("Penguins are assholes, no seriously f' those guys"); //Love, Jeffrey
 	}
-
+    	
     //Christian Testttt
 	
 }
