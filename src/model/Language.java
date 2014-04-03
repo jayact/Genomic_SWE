a821aeacf23a9194359b26924c06cc40d1a581fe
arteag56 <arteag56@students.rowan.edu>
2014-04-03 17:41:00 -0400

    Language skelly

--- src/model/Language.java
+++ src/model/Language.java
@@ -1,9 +1,18 @@
 package model;
+import java.util.*;
 
 /**
- *
  * @author Chris Meyers
  */
 public class Language {
+	public Map<String,String> language;
+	Language_Parser parser = new Language_Parser();
     
+	public String findString(String label) {
+		return "";
+	}
+	
+	public boolean setLanguage(String language) {
+		return false;
+	}
 }
