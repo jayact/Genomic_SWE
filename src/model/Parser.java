d9f145ab9be6179b064b2f46f174dbc83512dde6
christopher-meyers <cm.02.93@gmail.com>
2014-04-10 17:32:40 -0400

    Updated Parser class with primary functions.

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -2,10 +2,28 @@ package model;
 import java.util.*;
 /**
  *
- * @author Chris Meyers, Michael Arteaga
+ * @author Chris Meyers
  */
 public class Parser {
 	//String location;
 	
-	public boolean setLocation(String a);
+        public Parser(){
+        
+        }
+        
+        public Map<String, String> readGene(String path){
+            return null;
+        }
+        
+        public boolean writeResults(Map<String, List<Gene>>,String path){
+            return true;
+        }
+        
+        public Map<String disease, List<Gene gene>> readDisease(){
+            return null;
+        }
+        
+        public Map<String label, String phrase> readLanguage(){
+            return null;
+        }
 }
