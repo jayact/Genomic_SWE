60db64cac4722a921c8f320ede4f5aa983f6413b
jayact <jayact13@gmail.com>
2014-04-16 09:45:50 -0400

    adding adding getAffected to disease

--- src/model/Disease.java
+++ src/model/Disease.java
@@ -45,6 +45,46 @@ public class Disease
 	}
 	
 	/**
+	 * Gets a list of the affected genes.
+	 * @param selected is the list of selected genes.
+	 * @return the list of affected genes.
+	 */
+	public static List<List<Gene>> getAffected(List<Gene> selected)
+	{
+		List<List<Gene>> results = new List<List<Gene>>();
+		for(List<Gene> x : causes)
+		{
+			boolean conditions = true;
+			//check if the list of conditions are met.
+			int j = 0;
+			while(conditions == true && j < causes.size())
+			{
+				boolean gene_found = false;
+				int i = 0;
+				//check input list against condision
+				while(gene_found == false && i < selected.size())
+				{
+					if(gene_x.equals(selected.get(i)))
+					{
+						gene_found = true;
+					}
+					i++;
+				}
+				if(gene_found == false)
+				{
+					onditions = false;	
+				}
+				j++;
+			}
+			if(conditions == true)
+			{
+				results.add(x);
+			}
+		}
+		return results;
+	}
+	
+	/**
 	 * Gets the name of the disease.
 	 * @return the name of the disease.
 	 */
