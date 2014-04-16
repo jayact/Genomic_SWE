47503a81948f08fe0b928170579dc0100e415931
domio1992 <domio19922@gmail.com>
2014-04-16 09:33:34 -0400

    Update Handler

--- src/model/Handler.java
+++ src/model/Handler.java
@@ -3,69 +3,93 @@ package model;
 import java.util.*;
 
 public class Handler {
-	
+    
 	Map<String, Gene> data;
 	Map<String, Gene> selected;
 	Map<String, List<Gene>> disease;
-	
-    public boolean storeData(Map<String, Gene> map)
-    {
-        this.data = map;
-        return false;
-        
-    }
     
-    public boolean storeDisease (Map<String, List<Gene>> map){
+	public boolean storeData(Map<String, Gene> map)
+	{
+		this.data = map;
+		return false;
         
-        this.disease = map;
-        return false;
-    }
-    //
-    public Gene getType(String gene){
-        Gene temp = data.get(gene);
-        
-        return temp;
-    }
+	}
     
-    public boolean setType(Gene gene){
+	public boolean storeDisease (Map<String, List<Gene>> map)
+	{
         
+		this.disease = map;
+		return false;
+	}
+	//
+	public Gene getType(String gene)
+	{
+		Gene temp = data.get(gene);
         
-        return false;
-    }
+		return temp;
+	}
     
-    public boolean selectGene(String name){
+	public boolean setType(Gene gene)
+	{
+		if(gene != null)
+		{
+			Gene temp = data.get(gene.getName());
+			temp.setName(gene.getName());
+			return true;
+		}
         
-        return false;
-    }
+		return false;
+	}
     
-    public boolean deselectGene(String name){
+	public boolean selectGene(String name)
+	{
+		if(name != null)
+		{
+			Gene temp = data.get(name);
+			selected.put(name, temp);
+			return true;
+		}
         
-        return false;
-    }
+		return false;
+	}
     
-    public boolean deselectAll(){
+	public boolean deselectGene(String name)
+	{
         
-        return false;
-    }
+		if(name != null)
+		{
+			selected.remove(name);
+			return true;
+		}
+        
+		return false;
+	}
     
-    public Map<String, Gene> getData(){
-        return data;
-        
-    }
+	public boolean deselectAll()
+	{
+		selected.clear();
+		return false;
+	}
     
-    public Map<String, List<Gene>> getResults(){
+	public Map<String, Gene> getData()
+	{
         
-        
-        return disease;
-        
-    }
+		return data;
+	}
     
-    public Map<String, Gene> getSelected(){
+	public Map<String, List<Gene>> getResults()
+	{
         
+		return disease;
         
-        return data;
+	}
+    
+	public Map<String, Gene> getSelected()
+	{
         
-    }
+		return selected;
+        
+	}
     
     
 }
