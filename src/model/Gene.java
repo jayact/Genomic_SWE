c3fdbd0127216a80820e52ac91e2163b113a49aa
Derick Palos <DerickPalos93@gmail.com>
2014-04-23 10:35:00 -0400

    Added getters to Gene class and source formatted

--- src/model/Gene.java
+++ src/model/Gene.java
@@ -1,84 +1,115 @@
 package model;
 
-public class Gene
-{
+public class Gene {
 	private String urgency = "";
 	private String name = "";
 	private String type = "";
-	private String[] acceptedColors = {"red", "yellow", "green", "blue", "purple"};
-	private String[] acceptedTypes = {"heterozygous", "homozygous", "wild", "present"};
+	private String[] acceptedColors = { "red", "yellow", "green", "blue",
+			"purple" };
+	private String[] acceptedTypes = { "heterozygous", "homozygous", "wild",
+			"present" };
 
 	/**
 	 * Constructs a gene with all attributes
-	 * @param name - Name of the gene
-	 * @param type - Heterozygous, homozygous, wild, or present
-	 * @param color - Urgency rating: red, yellow, green, blue, purple
+	 * 
+	 * @param name
+	 *            Name of the gene
+	 * @param type
+	 *            Heterozygous, homozygous, wild, or present
+	 * @param color
+	 *            Urgency rating: red, yellow, green, blue, purple
 	 */
-	public Gene(String name, String type, String color)
-	{
+	public Gene(String name, String type, String color) {
 		this.name = name;
 		this.type = type;
 		this.urgency = color;
 	}
-	
+
 	/**
 	 * Constructs a gene with just the name
-	 * @param name - Name of the gene
+	 * 
+	 * @param name
+	 *            Name of the gene
 	 */
-	public Gene(String name)
-	{
+	public Gene(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * @return type
+	 */
+	public String getType() {
+		return this.type;
+	}
+
 	/**
 	 * Sets the type of the gene (heterozygous, homozygous, wild, or present)
-	 * @param type - 1 of the 4 types of genes
-	 * @return true if setting type is successful
-	 * 	false if setting type is unsuccessful
+	 * 
+	 * @param type
+	 *            1 of the 4 types of genes
+	 * @return true if setting type is successful false if setting type is
+	 *         unsuccessful
 	 */
-	public boolean setType(String type)
-	{
-		if(containsParam(acceptedTypes, type))
+	public boolean setType(String type) {
+		if (containsParam(acceptedTypes, type))
 			return true;
 		return false;
 	}
-	
+
 	/**
-	 * Sets the urgency color code of the gene (red, yellow, green, blue, or purple)
-	 * @param color - Final urgency color code of the gene
-	 * @return true if setting color is successful
-	 * 	false if setting color is unsuccessful
+	 * @return urgency
 	 */
-	public boolean setUrgency(String color)
-	{
-		if(containsParam(acceptedColors, color))
+	public String getUrgency() {
+		return this.urgency;
+	}
+
+	/**
+	 * Sets the urgency color code of the gene (red, yellow, green, blue, or
+	 * purple)
+	 * 
+	 * @param color
+	 *            Final urgency color code of the gene
+	 * @return true if setting color is successful false if setting color is
+	 *         unsuccessful
+	 */
+	public boolean setUrgency(String color) {
+		if (containsParam(acceptedColors, color))
 			return true;
 		return false;
 	}
-	
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
 	/**
 	 * Sets the name of the gene
-	 * @param name - Name of the gene
+	 * 
+	 * @param name
+	 *            Name of the gene
 	 * @return true if setting name is successful
 	 */
-	public boolean setName(String name)
-	{
+	public boolean setName(String name) {
 		this.name = name;
 		return true;
 	}
-	
+
 	/**
 	 * Checks if a String array contains a string
-	 * @param array - Array to check
-	 * @param check - String to check for membership
-	 * @return true if array contains check
-	 * 	fales if array does not contain check
+	 * 
+	 * @param array
+	 *            Array to check
+	 * @param check
+	 *            String to check for membership
+	 * @return true if array contains check fales if array does not contain
+	 *         check
 	 */
-	private boolean containsParam(String[] array, String check)
-	{
-		for(int i = 0; i < array.length; i++)
-			if(check.equalsIgnoreCase(array[i]))
-			{
+	private boolean containsParam(String[] array, String check) {
+		for (int i = 0; i < array.length; i++)
+			if (check.equalsIgnoreCase(array[i])) {
 				return true;
 			}
 		return false;
