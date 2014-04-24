b6a3d76ae11f49e0820e154380a9a15d30d1eb57
Jeffrey <creigh48@students.rowan.edu>
2014-04-24 16:59:04 -0400

    GUI modifications

---
+++ src/display/Display.java
@@ -0,0 +1,17 @@
+package display;
+import javax.swing.JApplet.*;
+/**
+ *
+ * @author Jeffrey
+ */
+public class Display {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+    
+}
