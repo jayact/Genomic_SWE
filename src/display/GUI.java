405685fdc1220cfbe8acef089eec7d535b0eafc0
jayact <jayact13@gmail.com>
2014-05-02 11:09:01 -0400

    removing gui main

--- src/display/GUI.java
+++ src/display/GUI.java
@@ -637,12 +637,12 @@ public class GUI extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+    /*public static void main(String args[]) {
+        // Set the Nimbus look and feel 
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        // If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         // For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         //
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -661,14 +661,14 @@ public class GUI extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        // Create and display the form //
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
 			public void run() {
                 new GUI().setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable available_gene_table;
