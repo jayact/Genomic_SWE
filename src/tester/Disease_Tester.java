da410357ab2330b9629f3ff81ab30b7b5581e362
arteag56 <arteag56@students.rowan.edu>
2014-05-03 16:51:42 -0400

    s

--- src/tester/Disease_Tester.java
+++ src/tester/Disease_Tester.java
@@ -25,11 +25,11 @@ public class Disease_Tester {
 		sickCause.add(sickList);
 		
 		Disease death = new Disease("Death","no heart beat", "no food", "none", "underground", deathCause);
-		/*Disease sick = new Disease("Sick","cough","lots of water","Medicine", "bed rest",sickCause);
+		Disease sick = new Disease("Sick","cough","lots of water","Medicine", "bed rest",sickCause);
 		
 		System.out.println(death + "\ngenes are caused of death");
 		
-*/
+
 	}
 
 }
