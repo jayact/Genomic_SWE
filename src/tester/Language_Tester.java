739339d0ebe54e2ea2b28f966b800bd33fc41487
arteag56 <arteag56@students.rowan.edu>
2014-05-03 14:36:26 -0400

    creating language test (again)

---
+++ src/tester/Language_Tester.java
@@ -0,0 +1,12 @@
+package tester;
+import java.util.HashMap;
+import model.Language;
+
+public class Language_Tester {
+	
+	public static void main(String[] args) {
+		Language english = new Language();
+		
+		
+	}
+}
