8c59b4013a8ef8c9e79f04664a5eecb5d9cef014
arteag56 <arteag56@students.rowan.edu>
2014-05-03 15:31:31 -0400

    gene tester

--- src/tester/Gene_Tester.java
+++ src/tester/Gene_Tester.java
@@ -3,16 +3,22 @@ import model.Gene;
 
 public class Gene_Tester {
 	public static void main(String[] args){
-		Gene g = new Gene("Simmons");		
-		System.out.println("Name:" + g.getName());
+		System.out.println("---GENE ONE---");
+		Gene g = new Gene("A-Gene");		
+		System.out.println("Name:" + g.getName() + "\n");
 		
-		System.out.println("---NEW GENE---");
-		Gene g2 = new Gene("A-Gene", "Heterozygous", "rNJx01", "HE", "Red");		
-		System.out.println(g2.toString());
+		System.out.println("---GENE TWO---");
+		Gene g2 = new Gene("B-Gene", "Heterozygous", "rNJx01", "HE", "Red");		
+		System.out.println("Name: " + g2.getName() + "\n");
 		
+		//CHANGES TO GENES
+		g.setRsNumber("rNJx00");
+		g.setUrgency("purple");
+		g.setAbbreviation("w");
 		g2.setType("wild");
 		
-		System.out.println("---SECOND GENE CHANGE---");
+		System.out.println("---ALL GENES PRINT---");
+		System.out.println(g.toString());
 		System.out.println(g2.toString());
 		
 	}
