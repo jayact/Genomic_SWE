e950485b3efe8ef8f72ef5b57d0d3b66fed2463a
arteag56 <arteag56@students.rowan.edu>
2014-05-03 14:39:57 -0400

    a

--- src/tester/Language_Tester.java
+++ src/tester/Language_Tester.java
@@ -7,6 +7,10 @@ public class Language_Tester {
 	public static void main(String[] args) {
 		Language english = new Language();
 		
+		HashMap<String,String> englDic = new HashMap<String,String>();
+		englDic.put("button1", "OK");
+		englDic.put("button2", "Cancel");
+		
 		
 	}
 }
